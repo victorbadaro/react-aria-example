54838b41d304be06df3e9e311c3ecd4ee5f85636
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-09-18 18:14:19 -0300

    feat: add tailwindcss dependency and its configs

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,7 @@
 export function App() {
-  return <h1>Hello, React Aria!</h1>;
-};
+	return (
+		<div className="flex items-center justify-center h-screen bg-zinc-950 text-zinc-50">
+			<h1>Hello, React Aria!</h1>
+		</div>
+	);
+}
